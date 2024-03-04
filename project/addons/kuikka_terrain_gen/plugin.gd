a549e4b57d9c34df442dd2d1eac99a6c3a5902fe
Sipi Ylä-Nojonen <seip.nojonen@gmail.com>
2024-03-04 18:58:44 +0200

    Initial structure for addon plugin

---
+++ project/addons/kuikka_terrain_gen/plugin.gd
@@ -0,0 +1,15 @@
+@tool
+extends EditorPlugin
+
+## Main [EditorPlugin] component for Kuikka Terrain Generator
+## to add / remove plugin from project.
+
+
+## Add components / enable plugin
+func _enter_tree():
+	pass
+	
+
+## Remove components / disable plugin
+func _exit_tree():
+	pass
